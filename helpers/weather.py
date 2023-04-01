516f0e21c2bde1f4bc31c2c091b4b21be324c806
Jcari88 <jasoncariolano@gmail.com>
2023-04-01 16:29:46 -0500

    send severe and daily updates*needs email input

--- helpers/weather.py
+++ helpers/weather.py
@@ -1,11 +1,11 @@
 import os
-
+from datetime import datetime, timezone
 import requests
 from geopy.geocoders import Nominatim
 
-from helpers.log import logger
+#from helpers.log import logger
 
-url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apikey}"
+url = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={apikey}"
 
 
 def get_coordinates(zipcode: str):
@@ -26,17 +26,71 @@ def get_weather(zipcode: str, mock: bool = False):
             'wind': {'speed': 4.63, 'deg': 30, 'gust': 8.23},
             'clouds': {'all': 40}, 'dt': 1679952325,
             'sys': {'type': 2, 'id': 2080868, 'country': 'US', 'sunrise': 1679918794, 'sunset': 1679963434},
-            'timezone': -18000, 'id': 4409896, 'name': 'Springfield', 'cod': 200
-        }
+            'timezone': -18000, 'id': 4409896, 'name': 'Springfield', 'cod': 200, }
     if location_details := get_coordinates(zipcode):
         address, latitude, longitude = location_details
     else:
-        logger.error("Failed to get location co-ordinations for the zipcode %s", zipcode)
+        #logger.error("Failed to get location co-ordinations for the zipcode %s", zipcode)
         return
     weather_url = url.format(lat=latitude, lon=longitude, apikey=os.environ.get("APIKEY"))
+    
     response = requests.get(url=weather_url)
-    if response.ok:
-        print(response.json())
+    #if response.ok:
+    #    print(response.json())
+
+    w = response.json()
+    #print(w)
+    today = datetime.now(timezone.utc).strftime('%Y-%m-%d')
+    todays_weather = next((d for d in w["daily"] if datetime.utcfromtimestamp(d["dt"]).strftime('%Y-%m-%d') == today), None)
+    #print(todays_weather)
+    todays_description = todays_weather["weather"][0]["description"]
+    todays_high= round((float(todays_weather["temp"]["max"])-273.15)*(9/5) + 32)#kelvin to faren
+    todays_low= round((float(todays_weather["temp"]["min"])-273.15)*(9/5) + 32)#kelvin to faren
+    
+    #print(todays_description, todays_high, todays_low)
+    
+    current_temperature = w["current"]["temp"]
+    current_pressure = w["current"]["pressure"]
+    current_humidity = w["current"]["humidity"]
+    
+    feels_like= w["current"]["feels_like"]
+
+    z = w["current"]["weather"]
+    weather_description = z[0]["description"]
+    current_temperatureF=round((float(current_temperature)-273.15)*(9/5) + 32)#kelvin to faren
 
 
-get_weather("65807", mock=True)
+    hourly = w["hourly"]
+    hourly_weather=[]
+    weather=[]
+    for entry in hourly:
+        temp = entry["temp"]
+        tempf= round((float(temp)-273.15)*(9/5) + 32)#kelvin to faren
+        dt= datetime.fromtimestamp( int(entry["dt"]))
+        dt = str(dt.strftime("%I:%M %p"))
+        h_weather_description= entry["weather"][0]["description"]
+        weather = [dt, tempf,"°F with", h_weather_description]
+        hourly_weather.append(weather)
+    
+    
+    weather_alerts = w.get("alerts")
+    
+    
+    
+    #print(weather_alerts)
+    #print( hourly_weather)
+    #print( current_temperatureF, weather_description)
+    return (hourly_weather, todays_description, todays_low, todays_high,weather_alerts)
+
+
+    
+    
+
+
+
+
+
+get_weather("65807", mock=False)
+
+
+
